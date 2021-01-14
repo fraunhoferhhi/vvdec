fc16c2541b4d05ddb9a3625c4a17f5bc56f75a57
Gabriel Hege <gabriel.hege@hhi.fraunhofer.de>
2021-01-14 14:25:52 +0100

    fix build for old GCC-version

--- source/Lib/CommonLib/x86/IntraPredX86.h
+++ source/Lib/CommonLib/x86/IntraPredX86.h
@@ -433,6 +433,7 @@ void IntraPredAngleCore_SIMD(int16_t* pDstBuf,const ptrdiff_t dstStride,int16_t*
 }
 
 #define _mm_storeu_si32(p, a) (void)(*(int*)(p) = _mm_cvtsi128_si32((a)))
+#define _mm_loadu_si64(p) _mm_cvtsi64_si128( *(int64_t*)p )
 #define _mm_loadu_si32(p) _mm_cvtsi32_si128( *(int32_t*)p )
 
 template< X86_VEXT vext, int W >
