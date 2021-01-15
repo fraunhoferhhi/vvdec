9f07b45f7ffe339d6aa54565849cf84edd832720
Gabriel Hege <gabriel.hege@hhi.fraunhofer.de>
2021-01-15 09:52:02 +0100

    convert fixed missing intrinsics to functions (instead of macros)
    and fix checking for intrinsics with GCC

--- cmake/modules/check_missing_intrinsics.cmake
+++ cmake/modules/check_missing_intrinsics.cmake
@@ -1,9 +1,9 @@
-include(CheckSymbolExists)
+include(CheckCSourceCompiles)
 
-check_symbol_exists( _mm_storeu_si32 "immintrin.h" HAVE_INITIN_mm_storeu_si32 )
-check_symbol_exists( _mm_storeu_si64 "immintrin.h" HAVE_INITIN_mm_storeu_si64 )
-check_symbol_exists( _mm_loadu_si32  "immintrin.h" HAVE_INITIN_mm_loadu_si32  )
-check_symbol_exists( _mm_loadu_si64  "immintrin.h" HAVE_INITIN_mm_loadu_si64  )
+check_c_source_compiles( "#include <immintrin.h>\n int main() { int32_t a = 0; __m128i x = _mm_setzero_si128(); _mm_storeu_si32( &a, x ); return 0; }" HAVE_INITIN_mm_storeu_si32 )
+check_c_source_compiles( "#include <immintrin.h>\n int main() { int64_t a = 0; __m128i x = _mm_setzero_si128(); _mm_storeu_si64( &a, x ); return 0; }" HAVE_INITIN_mm_storeu_si64 )
+check_c_source_compiles( "#include <immintrin.h>\n int main() { int32_t a = 0; __m128i x =                      _mm_loadu_si32( &a );     return 0; }" HAVE_INITIN_mm_loadu_si32  )
+check_c_source_compiles( "#include <immintrin.h>\n int main() { int64_t a = 0; __m128i x =                      _mm_loadu_si64( &a );     return 0; }" HAVE_INITIN_mm_loadu_si64  )
 
 macro( add_compile_definition_if_var VAR )
   if( ${VAR} )
