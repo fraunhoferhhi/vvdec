a6fad49a38cdf78928f3c8c2c49b2cf00e46d0e9
Gabriel Hege <gabriel.hege@hhi.fraunhofer.de>
2021-01-15 10:49:20 +0100

    fix missing intrinsics detection for MSVC

--- cmake/modules/check_missing_intrinsics.cmake
+++ cmake/modules/check_missing_intrinsics.cmake
@@ -1,17 +1,22 @@
 include(CheckCSourceCompiles)
 
-check_c_source_compiles( "#include <immintrin.h>\n int main() { int32_t a = 0; __m128i x = _mm_setzero_si128(); _mm_storeu_si32( &a, x ); return 0; }" HAVE_INITIN_mm_storeu_si32 )
-check_c_source_compiles( "#include <immintrin.h>\n int main() { int64_t a = 0; __m128i x = _mm_setzero_si128(); _mm_storeu_si64( &a, x ); return 0; }" HAVE_INITIN_mm_storeu_si64 )
-check_c_source_compiles( "#include <immintrin.h>\n int main() { int32_t a = 0; __m128i x =                      _mm_loadu_si32( &a );     return 0; }" HAVE_INITIN_mm_loadu_si32  )
-check_c_source_compiles( "#include <immintrin.h>\n int main() { int64_t a = 0; __m128i x =                      _mm_loadu_si64( &a );     return 0; }" HAVE_INITIN_mm_loadu_si64  )
+macro( check_intrinsic symbol_name code )
+  check_c_source_compiles("
+    #include <immintrin.h>
+    #include <stdint.h>
+    int main()
+    {
+      ${code}
+      return 0;
+    }"
+    HAVE_INITIN${symbol_name} )
 
-macro( add_compile_definition_if_var VAR )
-  if( ${VAR} )
-    add_compile_definitions( ${VAR} )
+  if( HAVE_INITIN${symbol_name} )
+    add_compile_definitions( HAVE_INITIN${symbol_name} )
    endif()
 endmacro()
 
-add_compile_definition_if_var( HAVE_INITIN_mm_storeu_si32 )
-add_compile_definition_if_var( HAVE_INITIN_mm_storeu_si64 )
-add_compile_definition_if_var( HAVE_INITIN_mm_loadu_si32  )
-add_compile_definition_if_var( HAVE_INITIN_mm_loadu_si64  )
+check_intrinsic( _mm_storeu_si32 "int32_t a = 0; __m128i x = _mm_setzero_si128(); _mm_storeu_si32( &a, x );" )
+check_intrinsic( _mm_storeu_si64 "int64_t a = 0; __m128i x = _mm_setzero_si128(); _mm_storeu_si64( &a, x );" )
+check_intrinsic( _mm_loadu_si32  "int32_t a = 0; __m128i x = _mm_setzero_si128(); x = _mm_loadu_si32( &a );" )
+check_intrinsic( _mm_loadu_si64  "int64_t a = 0; __m128i x = _mm_setzero_si128(); x = _mm_loadu_si64( &a );" )
