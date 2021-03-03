705b172cdbc003bf8f9e71115ce2033f84273bf2
Christian Lehmann <christian.lehmann@hhi.fraunhofer.de>
2021-03-03 08:56:52 +0100

    .

--- source/App/vvdecapp/vvdecapp.cpp
+++ source/App/vvdecapp/vvdecapp.cpp
@@ -190,7 +190,7 @@ int main( int argc, char* argv[] )
       return -1;
     }
 
-    vvdec_set_logging_callback( dec, msgFnc, nullptr, params.logLevel );
+    vvdec_set_logging_callback( dec, msgFnc );
 
     if( iLoop == 0 && params.logLevel >= VVDEC_VERBOSE )
     {
