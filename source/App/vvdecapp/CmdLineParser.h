b5c82216de89c2222076fffbc312d9ef1e472623
yuanhecai <yuanhecai@loongson.cn>
2025-01-07 10:23:14 +0800

    loongarch: add loongarch support

--- source/App/vvdecapp/CmdLineParser.h
+++ source/App/vvdecapp/CmdLineParser.h
@@ -400,6 +400,8 @@ public:
             case VVDEC_SIMD_NEON:      cll = "NEON";      break;
 #elif defined (VVDEC_ARCH_WASM)
             case VVDEC_SIMD_WASM:      cll = "WASM-SIMD"; break;
+#elif defined (VVDEC_ARCH_LOONGARCH)
+            case VVDEC_SIMD_LSX:       cll = "LSX";       break;
 #else
             case VVDEC_SIMD_SIMDE_ANY: cll = "SIMDE-ANY"; break;
 #endif
