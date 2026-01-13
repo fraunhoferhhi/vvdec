f21474f37a209fdfde8f536fcfb9d0ee2535047a
Gabriel Hege <gabriel.hege@hhi.fraunhofer.de>
2026-01-13 11:45:36 +0100

    update copyright statements to 2026

--- source/Lib/CommonLib/arm/neon/sum_neon.h
+++ source/Lib/CommonLib/arm/neon/sum_neon.h
@@ -6,7 +6,7 @@ the Software are granted under this license.
 
 The Clear BSD License
 
-Copyright (c) 2019-2025, Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V. & The VVDeC Authors.
+Copyright (c) 2019-2026, Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V. & The VVDeC Authors.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
